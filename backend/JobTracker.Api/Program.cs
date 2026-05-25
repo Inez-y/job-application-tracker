c05d2c2c0e64bdb8c668d83fa594274c886aae2f
Inez Yoon <inez3cs@gmail.com>
2026-05-24 19:46:00 -0700

    Create input validations

--- backend/JobTracker.Api/Program.cs
+++ backend/JobTracker.Api/Program.cs
@@ -6,10 +6,15 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.OpenApi.Models;
+using FluentValidation;
+using FluentValidation.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
+builder.Services.AddFluentValidationAutoValidation();
+builder.Services.AddValidatorsFromAssemblyContaining<Program>();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
